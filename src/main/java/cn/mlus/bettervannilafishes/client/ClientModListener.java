185592417a75243c02ddb5e5eb8c37e6beffe839
mlus <1319237806@qq.com>
2026-01-12 14:31:12 +0800

    fix #1

--- src/main/java/cn/mlus/bettervannilafishes/client/ClientModListener.java
+++ src/main/java/cn/mlus/bettervannilafishes/client/ClientModListener.java
@@ -8,12 +8,13 @@ import cn.mlus.bettervannilafishes.client.render.entity.BvfRenderer;
 import cn.mlus.bettervannilafishes.client.render.entity.BvfSpecimenRenderer;
 import cn.mlus.bettervannilafishes.init.BvfBlockEntities;
 import cn.mlus.bettervannilafishes.init.BvfEntities;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 
 
-@EventBusSubscriber
+@EventBusSubscriber(Dist.CLIENT)
 public final class ClientModListener {
 	@SubscribeEvent
 	public static void registerRenderers(final EntityRenderersEvent.RegisterRenderers event) {
