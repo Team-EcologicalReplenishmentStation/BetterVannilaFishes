0d18b502031ebe080bc708832e2aaf70fdef0071
mlus <1319237806@qq.com>
2026-01-30 19:05:19 +0800

    damage type registered

--- src/main/java/cn/mlus/bettervannilafishes/init/BvfDamageSource.java
+++ src/main/java/cn/mlus/bettervannilafishes/init/BvfDamageSource.java
@@ -10,13 +10,9 @@ import net.minecraft.world.level.Level;
 import java.util.Locale;
 
 public class BvfDamageSource {
-    public static final ResourceKey<DamageType> DIE_OF_BLEED;
+    public static final ResourceKey<DamageType> DIE_OF_BLEED = ResourceKey.create(Registries.DAMAGE_TYPE, BetterVannilaFishes.prefix("bleeding"));
 
     public static DamageSource getDamageSource(Level level, ResourceKey<DamageType> type) {
         return new DamageSource(level.registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(type), null, null);
-    }
-
-    static {
-        DIE_OF_BLEED = ResourceKey.create(Registries.DAMAGE_TYPE, BetterVannilaFishes.prefix("bleeding".toLowerCase(Locale.ROOT)));
     }
 }
