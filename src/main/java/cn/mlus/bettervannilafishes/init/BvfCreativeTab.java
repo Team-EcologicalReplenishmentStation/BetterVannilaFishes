35ef42a6bca157e68f6bde600e396933a23e4519
mlus <1319237806@qq.com>
2026-04-18 15:05:01 +0800

    Reorganize spawn egg and bucket entries for Channa and Elopichthys species in BvfCreativeTab

--- src/main/java/cn/mlus/bettervannilafishes/init/BvfCreativeTab.java
+++ src/main/java/cn/mlus/bettervannilafishes/init/BvfCreativeTab.java
@@ -48,9 +48,6 @@ public class BvfCreativeTab {
                output.accept(BvfItems.AEQUIDENS_TETRAMERUS_SPAWN_EGG.get());
                output.accept(BvfItems.GYMNOGEOPHAGUS_BALZANII_SPAWN_EGG.get());
                output.accept(BvfItems.ASTRONOTUS_OCELLATUS_SPAWN_EGG.get());
-               output.accept(BvfItems.CHANNA_MICROPELTES_SPAWN_EGG.get());
-               output.accept(BvfItems.CHANNA_ARGUS_SPAWN_EGG.get());
-               output.accept(BvfItems.ELOPICHTHYS_BAMBUSA_SPAWN_EGG.get());
                output.accept(BvfItems.AMPHIPRION_OCELLARIS_SPAWN_EGG.get());
                output.accept(BvfItems.PREMNAS_BIACULEATUS_SPAWN_EGG.get());
                output.accept(BvfItems.AMPHIPRION_POLYMNUS_SPAWN_EGG.get());
@@ -58,6 +55,9 @@ public class BvfCreativeTab {
                output.accept(BvfItems.AMPHIPRION_CLARKII_SPAWN_EGG.get());
                output.accept(BvfItems.AMPHIPRION_PERCULA_SPAWN_EGG.get());
                output.accept(BvfItems.DOSIDICUS_GIGAS_SPAWN_EGG.get());
+               output.accept(BvfItems.CHANNA_MICROPELTES_SPAWN_EGG.get());
+               output.accept(BvfItems.CHANNA_ARGUS_SPAWN_EGG.get());
+               output.accept(BvfItems.ELOPICHTHYS_BAMBUSA_SPAWN_EGG.get());
                 //Bucket
                output.accept(BvfItems.HADDOCK_COD_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
                output.accept(BvfItems.ATLANTIC_COD_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
@@ -93,9 +93,6 @@ public class BvfCreativeTab {
                output.accept(BvfItems.AEQUIDENS_TETRAMERUS_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
                output.accept(BvfItems.GYMNOGEOPHAGUS_BALZANII_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
                output.accept(BvfItems.ASTRONOTUS_OCELLATUS_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-               output.accept(BvfItems.CHANNA_MICROPELTES_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-               output.accept(BvfItems.CHANNA_ARGUS_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-               output.accept(BvfItems.ELOPICHTHYS_BAMBUSA_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
                output.accept(BvfItems.AMPHIPRION_OCELLARIS_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
                output.accept(BvfItems.PREMNAS_BIACULEATUS_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
                output.accept(BvfItems.AMPHIPRION_POLYMNUS_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
@@ -103,6 +100,9 @@ public class BvfCreativeTab {
                output.accept(BvfItems.AMPHIPRION_CLARKII_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
                output.accept(BvfItems.AMPHIPRION_PERCULA_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
                output.accept(BvfItems.DOSIDICUS_GIGAS_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+               output.accept(BvfItems.CHANNA_MICROPELTES_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+               output.accept(BvfItems.CHANNA_ARGUS_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+               output.accept(BvfItems.ELOPICHTHYS_BAMBUSA_BUCKET.get().getDefaultInstance(), CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
                 //Food
                output.accept(BvfItems.HADDOCK_COD.get());
                output.accept(BvfItems.ATLANTIC_COD.get());
@@ -139,11 +139,6 @@ public class BvfCreativeTab {
                output.accept(BvfItems.AEQUIDENS_TETRAMERUS.get());
                output.accept(BvfItems.GYMNOGEOPHAGUS_BALZANII.get());
                output.accept(BvfItems.ASTRONOTUS_OCELLATUS.get());
-               output.accept(BvfItems.CHANNA_MICROPELTES.get());
-               output.accept(BvfItems.CHANNA_ARGUS.get());
-               output.accept(BvfItems.COOKED_CHANNA.get());
-               output.accept(BvfItems.ELOPICHTHYS_BAMBUSA.get());
-               output.accept(BvfItems.COOKED_ELOPICHTHYS_BAMBUSA.get());
                output.accept(BvfItems.AMPHIPRION_OCELLARIS.get());
                output.accept(BvfItems.PREMNAS_BIACULEATUS.get());
                output.accept(BvfItems.AMPHIPRION_POLYMNUS.get());
@@ -153,6 +148,11 @@ public class BvfCreativeTab {
                output.accept(BvfItems.HUMBOLDT_SQUID.get());
                output.accept(BvfItems.COOKED_HUMBOLDT_SQUID.get());
                output.accept(BvfItems.FEATHER_STAR_TENTACLE.get());
+               output.accept(BvfItems.CHANNA_MICROPELTES.get());
+               output.accept(BvfItems.CHANNA_ARGUS.get());
+               output.accept(BvfItems.COOKED_CHANNA.get());
+               output.accept(BvfItems.ELOPICHTHYS_BAMBUSA.get());
+               output.accept(BvfItems.COOKED_ELOPICHTHYS_BAMBUSA.get());
                 //Specimen
                output.accept(BvfItems.HADDOCK_COD_SPECIMEN.get());
                output.accept(BvfItems.ATLANTIC_COD_SPECIMEN.get());
@@ -171,9 +171,9 @@ public class BvfCreativeTab {
                output.accept(BvfItems.AEQUIDENS_TETRAMERUS_SPECIMEN.get());
                output.accept(BvfItems.GYMNOGEOPHAGUS_BALZANII_SPECIMEN.get());
                output.accept(BvfItems.ASTRONOTUS_OCELLATUS_SPECIMEN.get());
-                    output.accept(BvfItems.CHANNA_MICROPELTES_SPECIMEN.get());
-                    output.accept(BvfItems.CHANNA_ARGUS_SPECIMEN.get());
-                     output.accept(BvfItems.ELOPICHTHYS_BAMBUSA_SPECIMEN.get());
+               output.accept(BvfItems.CHANNA_MICROPELTES_SPECIMEN.get());
+               output.accept(BvfItems.CHANNA_ARGUS_SPECIMEN.get());
+               output.accept(BvfItems.ELOPICHTHYS_BAMBUSA_SPECIMEN.get());
             }).build());
     
     public static void register(IEventBus eventBus) {
